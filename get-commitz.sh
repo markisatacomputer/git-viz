312738639dff8e52edd5e45752438fe4e8b611a6
Mark N <mark.is.at.a.computer@gmail.com>
2020-03-09 12:54:58 -0700

    make author optional.  output more columns

--- get-commitz.sh
+++ get-commitz.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 #
-#  Parse author arg
-AUTHOR=$1
-shift
-
-PWD=$(pwd)
-
-
-#   get it all in csv form:
+#   USAGE:
+#     ./get-commitz.sh '\(Name\sIsh\)|\email@ish.com\' path/to/repo path/to/repo2
+#
+#   ARGS:
+#     - author: (optional) filter by author regex string
+#     - repo path:
+#
+#   OUTPUT:
+#     All commits from current directory git repo in csv form:
 #     -  author name, commit hash short, author timestamp, commit timestamp, files changed, additions, deletions
 #
+#
 function get_commits() {
+  #  store repo title extracted from folder name
   local PWD=$(pwd)
   local repo=${PWD//\// }
   repo=$(echo $repo | awk '{print $NF}')
-  git log --author=$AUTHOR --format=format:"%an, %h, %at, %ct" --shortstat --no-merges -n 10 | sed -E \
+
+  #  the command
+  git log --author="$1" --format=format:"%an, %H, %at, %ct, \"%D\", \"%s\"" --shortstat --no-merges | sed -E \
   '/./{H;$!d;} ;
   x ;
   s/ ([0-9]+) files* changed, ([0-9]+) insertions*\(\+\), ([0-9]+) deletions*\(-\)/, \1, \2, \3, '$repo'/ ;
@@ -23,16 +28,22 @@ function get_commits() {
   s/\n//g'
 }
 
-#  Init header row
-echo "author, commit hash, author time, commit time, files changed, insertions, deletions, repository"
+if [ ! -d "$1" ];  then
+  AUTHOR="$1"
+  shift
+else
+  AUTHOR=".*"
+fi
 
-#  Go to all
+#   Save Directory
+PWD=$(pwd)
+
+#  Cycle through all git repo dirs and add rows
 for var in "$@"
 do
   cd $var
-  get_commits
+  get_commits "$AUTHOR"
 done
 
+#  Return to initial directory
 cd $PWD
-
-echo $AllCommits
